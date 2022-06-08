786845eda05f41e471a9859c5a5ac77671c266bf
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-06-09 01:12:58 +0600

    home

--- src/ProtfilioContainer/Home/Profilo.js
+++ src/ProtfilioContainer/Home/Profilo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Typical from "react-typical"
 
 const Profilo = () => {
     return (
@@ -6,7 +7,7 @@ const Profilo = () => {
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='colz'>
-                        <a href="#">
+                        <a href="https://www.facebook.com/md.mehadyhasanakash">
 
                             <i className="fa-brands fa-facebook-f"></i>
                         </a>
@@ -16,22 +17,67 @@ const Profilo = () => {
                             <i className='fa-brands fa-google-plus'></i>
                         </a>
 
-                        <a href="#">
+                        <a href="https://www.instagram.com/hasanmdmehady/">
 
                             <i className='fa-brands fa-instagram-square'></i>
                         </a>
 
-                        <a href="#">
+                        <a href="https://www.youtube.com/channel/UCD_yUavpJBzoB6oE8xI4LjA">
 
                             <i className='fa-brands fa-youtube'></i>
                         </a>
 
-                        <a href="#">
+                        <a href="https://twitter.com/mehadyhasan1000">
 
                             <i className='fa-brands fa-twitter'></i>
                         </a>
 
                     </div>
+
+
+                    <div className='profile-details-name'>
+                        <span className='primary-text'>
+                            {""}
+                            Hello I'M <span className='highlighted-text'>Md Mehady Hasan Akash</span>
+                        </span>
+
+                    </div>
+
+                    <div className='profile-details-role'>
+                        <span className='primary-text'>
+                            {""}
+                            <h1>
+                                {""}
+                                <Typical
+                                    loop={Infinity}
+                                    steps={[
+                                        "Full Stack Developer",
+                                        1000,
+                                        "MERN Stack Developer",
+                                        1000,
+                                        "React/ReactNative Developer",
+                                        1000,
+                                    ]}
+
+                                />
+                            </h1>
+                            <span className='profile-role-tagline'>
+                                knock a building application with font and back end operation.
+
+                            </span>
+                        </span>
+                    </div>
+
+                    <div className='profile-option'>
+
+                        <button className='btn btn-primary'>
+
+                            {""}
+                            Hire me
+                        </button>
+                    </div>
+
+
                 </div>
 
             </div>
