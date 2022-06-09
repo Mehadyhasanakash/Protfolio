05b4b8b2299d9d11caaecb9d6f277e63a36dec13
Mehadyhasanakash <mehadyhasanakash@gmail.com>
2022-06-09 15:16:44 +0600

    final home page creat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Profilo from "./ProtfilioContainer/Home/Profilo";
 import './App.css'
 import About from "./ProtfilioContainer/About/About";
+import Nav from "./ProtfilioContainer/Nav/Nav";
 
 function App() {
   return (
     <div className="App" >
+      <Nav/>
       <Profilo/>
       <About/>
     </div>
--- src/ProtfilioContainer/About/About.js
+++ src/ProtfilioContainer/About/About.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import image from '../../assets/Home/Abdul hamid.jpg'
 
 const About = () => {
     return (
-        <div class="hero min-h-screen bg-base-200 ">
+        <div class="hero min-h-92 bg-base-200  ">
   <div class="hero-content flex-col lg:flex-row-reverse">
     <div class="text-center lg:text-left">
-      <h1 class="text-5xl font-bold text-purple-900">ABOUT ME</h1>
-      <p class="py-6 text-2xl">Full stack web and mobile developer with background knowledge of MERN stacks with redux, along with a knack of building applications with utmost efficiency. Strong professional with a BSC willing to be an asset for an organization.</p>
+      <h1 class="text-5xl font-bold text-orange-500 mt-10">ABOUT ME</h1>
+      <h1 className='font-bold text-3xl mt-10'>Why Chosse Me</h1>
+      <p class="py-6 text-2xl   ">Full stack web and mobile developer with background knowledge of MERN stacks with redux, along with a knack of building applications with utmost efficiency. Strong professional with a BSC willing to be an asset for an organization.</p>
       <h1 className='text-xl'>Here are a Few Highlights:</h1>
       <p>1.Full Stack web and mobile development </p>
       <p>2. Interactive Front End as per the design</p>
@@ -19,6 +21,7 @@ const About = () => {
     </div>
     <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
       <div class="card-body">
+          <img src={image} alt="" />
         
         
         
